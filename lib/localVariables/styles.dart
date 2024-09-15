1e0c6937316e94f6b98912c1bccb0948ed163416
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-09-15 16:58:42 +0300

    product version 1

---
+++ lib/localVariables/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:aura/localVariables/local_variables.dart';
+var auraFontFayrozi40 =TextStyle(
+  color: firozi ,
+  fontSize: 40 ,
+);
