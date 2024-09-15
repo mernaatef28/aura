071f26a55b4a15de984ea399bf0122908ddae2dc
sylviaehab <silvyaehab@gmail.com>
2024-09-15 17:10:11 +0300

    AURA

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+    import 'package:flutter/material.dart';
 import 'package:finalproj/signupPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
