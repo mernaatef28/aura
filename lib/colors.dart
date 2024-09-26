0903c9a685b4df5aff3d6f31358613d874680cb9
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-09-27 02:30:43 +0300

    changes

--- lib/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color lightYellow = Color(0xFFF8F0AE);
-  static const Color lightBlue = Color(0xFFBBD8E2);
-  static const Color tealBlue = Color(0xFF51858C);
-  static const Color lightPink = Color(0xFFF6CAC9);
-}
